9f16879a941334087f8670916d57c673bf85e5d3
TimRathert <rathertt@gmail.com>
2022-09-26 14:48:33 -0400

    steps

---
+++ python/climbing-stairs.py
@@ -0,0 +1,14 @@
+# 2 > 2 options
+# 3 > 3 options
+# 4 > 5 options
+# 5 > 8 options 
+# 6 > 13 options
+# fibonacci!
+
+# 3//2 = 1
+# 3%2 = 1
+
+def climbStairs(n):
+    steps = 0
+    for idx,num in range(1, n):
+
