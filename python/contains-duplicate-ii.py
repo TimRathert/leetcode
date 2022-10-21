23f2a71ffcf69f0b28bbbe3125c00ec12cdf0226
TimRathert <rathertt@gmail.com>
2022-10-21 13:09:21 -0400

    got it using defaultdict

--- python/contains-duplicate-ii.py
+++ python/contains-duplicate-ii.py
@@ -1,13 +1,22 @@
+# def containsNearbyDuplicate(nums, k):
+#     for idx, numI in enumerate(nums):
+#         indexI = idx
+#         for idx, numJ in enumerate(nums):
+#             indexJ = idx
+#             if numI == numJ and indexI != indexJ and abs(indexI - indexJ) <= k:
+#                 return True
+#     return False
+
+from collections import defaultdict
+
+
 def containsNearbyDuplicate(nums, k):
-    for idx, numI in enumerate(nums):
-        indexI = idx
-        for idx, numJ in enumerate(nums):
-            indexJ = idx
-            if numI == numJ and indexI != indexJ and abs(indexI - indexJ) <= k:
-                return True
+    seen = defaultdict(int)
+    for i, num in enumerate(nums):
+        if num in seen and i-seen[num]<= k:
+            return True
+        seen[num] = i
     return False
-
-
     
 
 print(containsNearbyDuplicate([1,2,3,1], 3))
