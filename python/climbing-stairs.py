884104612686b5f1a59b0d3bcb6a6a7cdb39c358
TimRathert <rathertt@gmail.com>
2022-10-26 12:45:18 -0400

    climb stairs with recursion

--- python/climbing-stairs.py
+++ python/climbing-stairs.py
@@ -8,11 +8,20 @@
 # 3//2 = 1
 # 3%2 = 1
 
+# def climbStairs(n):
+#     steps = 0 #unneccessary
+#     arr = [1,1]
+#     for idx,num in enumerate(range(2, n+1)):
+#         arr.append(arr[num-1]+arr[num-2])
+#     return arr[-1]
+
 def climbStairs(n):
-    steps = 0
     arr = [1,1]
-    for idx,num in enumerate(range(2, n+1)):
-        arr.append(arr[num-1]+arr[num-2])
+    def recurse():
+        arr.append(arr[len(arr)-1]+arr[len(arr)-2])
+    while len(arr) < n+1:
+        recurse()
     return arr[-1]
+    
 
 print(climbStairs(6))
