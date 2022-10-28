3568e8ba1e78349c362f950772eb3011c0b12759
TimRathert <rathertt@gmail.com>
2022-10-28 14:42:16 -0400

    missing numbers

--- python/missing-number.py
+++ python/missing-number.py
@@ -1,6 +1,6 @@
 def missingNumber(nums):
     length = len(nums)
-    for i in range(0, len(nums), 1):
+    for i in range(len(nums)):
         try:
             nums.remove(i)
         except ValueError:
