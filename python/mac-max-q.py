d64e3f3d0b1a45aec4bdee788a0138c8e1f2f195
TimRathert <rathertt@gmail.com>
2022-10-28 11:42:48 -0400

    got it

--- python/mac-max-q.py
+++ python/mac-max-q.py
@@ -1,4 +1,5 @@
 from jinja2 import Undefined
+from numpy import sort
 
 
 def rearranged(int):
@@ -10,25 +11,24 @@ def rearranged(int):
         elif low2 == Undefined:
             low2 = num
         elif num < low1 or num < low2:
-            if num < low1:
-                low1 = num
-            elif num < low2:
-                low2 = num
-    print(low1, low2)
+            if low1 > low2:
+                if num < low1:
+                    low1 = num
+            elif low2 > low1:
+                if num < low2:
+                    low2 = num
     int.remove(low1)
+    int.remove(low2)
+    int.sort()
     int.insert(2,low1)
     if len(int) % 2 == 0:
-        int.remove(low2)
-        int.insert(len(int)-1,low2)
-    else:
-        int.remove(low2)
         int.insert(len(int),low2)
+    else:
+        int.insert(len(int)-1,low2)
     return int
-    
-    
         
-#print(rearranged([21, 34, 5, 7, 9]))
-print(rearranged([21, 34, 5, 7, 9, 15, 72, 4]))
+print(rearranged([21, 34, 5, 7, 9]))
+#print(rearranged([21, 34, 5, 7, 9, 15, 72, 4]))
 
     # U = 1
     # for index, num in enumerate(int):
