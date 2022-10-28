309925e12c55ec8b913bb61d87dc44d24f479cf2
TimRathert <rathertt@gmail.com>
2022-10-28 11:21:20 -0400

    maximizing array value

---
+++ python/mac-max-q.py
@@ -0,0 +1,44 @@
+from jinja2 import Undefined
+
+
+def rearranged(int):
+    low1 = Undefined
+    low2 = Undefined
+    for num in int:
+        if low1 == Undefined:
+            low1 = num
+        elif low2 == Undefined:
+            low2 = num
+        elif num < low1 or num < low2:
+            if num < low1:
+                low1 = num
+            elif num < low2:
+                low2 = num
+    print(low1, low2)
+    int.remove(low1)
+    int.insert(2,low1)
+    if len(int) % 2 == 0:
+        int.remove(low2)
+        int.insert(len(int)-1,low2)
+    else:
+        int.remove(low2)
+        int.insert(len(int),low2)
+    return int
+    
+    
+        
+#print(rearranged([21, 34, 5, 7, 9]))
+print(rearranged([21, 34, 5, 7, 9, 15, 72, 4]))
+
+    # U = 1
+    # for index, num in enumerate(int):
+    #     if index < 2:
+    #         U *= num
+    #     elif index == 2:
+    #         U /= num
+    #     elif index == len(int)-1:
+    #         if index % 2 == 1:
+    #             U /= num
+    #         if index %2 == 0:
+    #             U *= num
+
