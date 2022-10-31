fb15c966deb997c8ce94b15b4d75bc2ffa2018c2
TimRathert <rathertt@gmail.com>
2022-10-31 13:28:09 -0400

    group anagrams

--- python/group-anagrams.py
+++ python/group-anagrams.py
@@ -4,6 +4,8 @@ from collections import defaultdict
 def groupAnagrams(strs):
     if len(strs) == 0: return []
     letterObj = []
+    seen = defaultdict(dict)
+    output = []
     for word in strs:
         newObj = defaultdict(int)
         for letter in word:
@@ -11,12 +13,16 @@ def groupAnagrams(strs):
                 newObj[letter]= 1
             else: newObj[letter] += 1
         letterObj.append(newObj)
-    for obj in enumerate(letterObj):
-        for i in range(len(letterObj)):
+        if not seen[frozenset(newObj)]:
+            seen[frozenset(newObj)] = True
+            output.append([word])
+        if seen[frozenset(newObj)]:
+            output[seen.keys(frozenset(newObj))]
+
+
+    return output
+
             
-
-
-    return letterObj
 
 print(groupAnagrams( ["eat","tea","tan","ate","nat","bat"]))
 
