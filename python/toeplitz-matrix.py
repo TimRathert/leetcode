d793173f7c5642a9c9f65448704efb9ff8ceb3dd
TimRathert <rathertt@gmail.com>
2022-10-31 14:31:56 -0400

    completed toeplitz matrix with some gusto!

---
+++ python/toeplitz-matrix.py
@@ -0,0 +1,25 @@
+def isToeplitzMatrix(matrix):
+    # m = len(matrix)
+    # n = len(matrix[0])
+    # if m < len(matrix) and n < len(matrix[0]):
+    #     if matrix[m][n] != matrix[m+1][n+1]:
+    #         return False
+    # return True
+    i = 0
+    while i < len(matrix)-1:
+        holder = matrix[i][slice(0, len(matrix[0])-1, 1)]
+        compare = matrix[i+1][slice(1, len(matrix[0]), 1)]
+        if holder != compare:
+            return False
+        i += 1
+    return True
+
+        
+  # runtime 88ms - faster than 95.34%
+  # mem 13.8mb - less than 98.45%          
+  # WHOO!
+
+
+
+print(isToeplitzMatrix([[1,2,3,4],[5,1,2,3],[9,5,1,2]]))
+print(isToeplitzMatrix([[1,2],[2,2]]))
