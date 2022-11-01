ad17585663cfbcf4286464b730ab9b314aa59beb
TimRathert <rathertt@gmail.com>
2022-11-01 12:09:53 -0400

    find the ball is working except for this ridiculous test case lmao

--- python/where-will-the-ball-land.py
+++ python/where-will-the-ball-land.py
@@ -2,12 +2,50 @@ from jinja2 import Undefined
 
 
 def findBall(grid):
-    class Ball:
-        def __init__(self, startPos, neighbors, endPos):
-            self.startPos = startPos
-            self.neighbors = {}
-            self.endPos = Undefined
+    output = []
+    def eachBall(startIndex):
+        class Ball:
+            def __init__(self, startPos):
+                self.startPos = startPos
+                self.currentPos = startPos
+                self.endPos = None
+
+        newBall = Ball(startIndex)        
+        row = 0
+        var = None
+        while newBall.endPos == None:
+            try:
+                var = grid[row][newBall.currentPos] + grid[row][(newBall.currentPos) + grid[row][newBall.currentPos]]
+            except IndexError:
+                newBall.endPos = -1
+                if len(output) < len(grid[0]):
+                    output.append(newBall.endPos)
+            else:
+                if var == 0:
+                    newBall.endPos = -1
+                    if len(output) < len(grid[0]):
+                        output.append(newBall.endPos)
+                else:
+                    newBall.currentPos += grid[row][newBall.currentPos] 
+                    row += 1
+            if row == len(grid)-1:
+                newBall.currentPos += grid[row][newBall.currentPos] 
+                newBall.endPos = newBall.currentPos
+                if len(output) < len(grid[0]):
+                    output.append(newBall.endPos)
+                break
+                # print(newBall.startPos, newBall.currentPos)
+
+    for i in range(len(grid[0])):
+        eachBall(i)
+
+    return output
+
     
 
 
-print(findBall([[1,1,1,-1,-1],[1,1,1,-1,-1],[-1,-1,-1,1,1],[1,1,1,1,-1],[-1,-1,-1,-1,-1]]))
+# print(findBall([[1,1,1,-1,-1],[1,1,1,-1,-1],[-1,-1,-1,1,1],[1,1,1,1,-1],[-1,-1,-1,-1,-1]]))
+# print(findBall([[-1]]))
+# print(findBall([[1,1,1,1,1,1],[-1,-1,-1,-1,-1,-1],[1,1,1,1,1,1],[-1,-1,-1,-1,-1,-1]]))
+# print(findBall([[1]]))
+print(findBall([[-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,1,1,-1,-1,-1,1,1,1,-1,-1,1,1,-1,-1,1,-1,-1,-1,-1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,-1,-1,-1,1,-1,-1,1,-1,1,-1,-1,1,1,-1,1,-1,-1,-1,-1,1,1,1,1,1,1,-1,1,1,1,-1,1,1,1,-1,-1,-1,1,-1,1,-1,-1,1,1,-1,-1,1,-1,1,-1,1,1,1,-1,-1,-1,-1]]))
