8f815457689a15a0ee1f0648d013302c35c16452
TimRathert <rathertt@gmail.com>
2022-11-01 09:44:16 -0400

    adding notes

--- python/group-anagrams.py
+++ python/group-anagrams.py
@@ -8,19 +8,26 @@ def groupAnagrams(strs):
     output = []
     var = 0
     for word in strs:
+
         newObj = defaultdict(dict)
+        # using defaultdict prevents python from returning a keyerror if the key doesn't exist within that object (useful on line 23)
+
         for letter in word:
             if not newObj[letter]:
                 newObj[letter] = 1
             else: newObj[letter] += 1
         newObj = frozenset(newObj.items())
-        if not seen[newObj]:
+        # for each of the words in strs, a newObj is created
+        # the keys are the letters and the values are the number of times that letter appears
+
+        if not seen[newObj]: #if seen[newObj] doesn't exist, create it
+                            # this uses the key value pairs from newObj (containing letter frequency) as the key and True as the value
             seen[newObj] = True
-            letterObj[newObj] = var
-            var += 1
+            letterObj[newObj] = var  #var is the counter. I used this to track the index of the corresponding word in the output array
+            var += 1 # when a word is inserted, this increments to denote where the next item will be inserted
             output.append([word])
-        elif seen[newObj]:
-            keys = seen.keys()
+        elif seen[newObj]: #if line 23 is false (aka it already exists) then the index is read from letterObj[newObj] = var to determine where to insert the current word in the output array
+            keys = seen.keys() #not sure I need this...
             output[letterObj[newObj]].append(word)
 
 
