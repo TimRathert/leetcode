6314e50e7c4c3064dc4df58c6de267279db96981
TimRathert <rathertt@gmail.com>
2022-11-01 12:54:00 -0400

    i dont know why this is happening

--- python/where-will-the-ball-land.py
+++ python/where-will-the-ball-land.py
@@ -13,14 +13,21 @@ def findBall(grid):
         newBall = Ball(startIndex)        
         row = 0
         var = None
+        #print('started at ', newBall.startPos)
         while newBall.endPos == None:
             try:
-                var = grid[row][newBall.currentPos] + grid[row][(newBall.currentPos) + grid[row][newBall.currentPos]]
-            except IndexError:
+                y = ((newBall.currentPos) + grid[row][newBall.currentPos]) if newBall.startPos > 0 else 1
+                print (y)
+                grid[row][newBall.currentPos] + grid[row][y]
+                print('var is', var)
+            except IndexError as err:
                 newBall.endPos = -1
-                if len(output) < len(grid[0]):
-                    output.append(newBall.endPos)
+                output.append(newBall.endPos)
+                print('started at',newBall.startPos, 'ended at', newBall.endPos)
+                print(err)
             else:
+                y = ((newBall.currentPos) + grid[row][newBall.currentPos]) if newBall.startPos > 0 else 1
+                var = grid[row][newBall.currentPos] + grid[row][y]
                 if var == 0:
                     newBall.endPos = -1
                     if len(output) < len(grid[0]):
@@ -28,13 +35,15 @@ def findBall(grid):
                 else:
                     newBall.currentPos += grid[row][newBall.currentPos] 
                     row += 1
-            if row == len(grid)-1:
-                newBall.currentPos += grid[row][newBall.currentPos] 
-                newBall.endPos = newBall.currentPos
-                if len(output) < len(grid[0]):
-                    output.append(newBall.endPos)
-                break
-                # print(newBall.startPos, newBall.currentPos)
+            finally:
+                if row == len(grid)-1:
+                    newBall.currentPos += grid[row][newBall.currentPos] 
+                    newBall.endPos = newBall.currentPos
+                    if len(output) < len(grid[0]):
+                        output.append(newBall.endPos)
+                        #print('started at ', newBall.startPos, 'ended at ', newBall.endPos)
+                    break
+                    # print(newBall.startPos, newBall.currentPos)
 
     for i in range(len(grid[0])):
         eachBall(i)
@@ -44,8 +53,66 @@ def findBall(grid):
     
 
 
-# print(findBall([[1,1,1,-1,-1],[1,1,1,-1,-1],[-1,-1,-1,1,1],[1,1,1,1,-1],[-1,-1,-1,-1,-1]]))
-# print(findBall([[-1]]))
-# print(findBall([[1,1,1,1,1,1],[-1,-1,-1,-1,-1,-1],[1,1,1,1,1,1],[-1,-1,-1,-1,-1,-1]]))
-# print(findBall([[1]]))
+#print(findBall([[1,1,1,-1,-1],[1,1,1,-1,-1],[-1,-1,-1,1,1],[1,1,1,1,-1],[-1,-1,-1,-1,-1]]))
+#print(findBall([[-1]]))
+#print(findBall([[1,1,1,1,1,1],[-1,-1,-1,-1,-1,-1],[1,1,1,1,1,1],[-1,-1,-1,-1,-1,-1]]))
+#print(findBall([[1]]))
 print(findBall([[-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,1,1,-1,-1,-1,1,1,1,-1,-1,1,1,-1,-1,1,-1,-1,-1,-1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,-1,-1,-1,1,-1,-1,1,-1,1,-1,-1,1,1,-1,1,-1,-1,-1,-1,1,1,1,1,1,1,-1,1,1,1,-1,1,1,1,-1,-1,-1,1,-1,1,-1,-1,1,1,-1,-1,1,-1,1,-1,1,1,1,-1,-1,-1,-1]]))
+
+
+
+
+
+# def findBall(grid):
+#     output = []
+#     def eachBall(startIndex):
+#         class Ball:
+#             def __init__(self, startPos):
+#                 self.startPos = startPos
+#                 self.currentPos = startPos
+#                 self.endPos = None
+
+#         newBall = Ball(startIndex)        
+#         row = 0
+#         var = None
+#         #print('started at ', newBall.startPos)
+#         while newBall.endPos == None:
+#             try:
+#                 y = (newBall.currentPos) + grid[row][newBall.currentPos] if newBall.startPos > 0 or newBall.startPos < len(grid[0])-1 else 1
+#                 var = grid[row][newBall.currentPos] + grid[row][y]
+#                 #print(var)
+#             except IndexError:
+#                 newBall.endPos = -1
+#                 if len(output) < len(grid[0]):
+#                     output.append(newBall.endPos)
+#                     print('started at ', newBall.startPos, 'ended at ', newBall.endPos)
+#             else:
+#                 if var == 0:
+#                     newBall.endPos = -1
+#                     if len(output) < len(grid[0]):
+#                         output.append(newBall.endPos)
+#                 else:
+#                     newBall.currentPos += grid[row][newBall.currentPos] 
+#                     row += 1
+#             if row == len(grid)-1:
+#                 newBall.currentPos += grid[row][newBall.currentPos] 
+#                 newBall.endPos = newBall.currentPos
+#                 if len(output) < len(grid[0]):
+#                     output.append(newBall.endPos)
+#                     #print('started at ', newBall.startPos, 'ended at ', newBall.endPos)
+#                 break
+#                 # print(newBall.startPos, newBall.currentPos)
+
+#     for i in range(len(grid[0])):
+#         eachBall(i)
+
+#     return output
+
+    
+
+
+# #print(findBall([[1,1,1,-1,-1],[1,1,1,-1,-1],[-1,-1,-1,1,1],[1,1,1,1,-1],[-1,-1,-1,-1,-1]]))
+# #print(findBall([[-1]]))
+# #print(findBall([[1,1,1,1,1,1],[-1,-1,-1,-1,-1,-1],[1,1,1,1,1,1],[-1,-1,-1,-1,-1,-1]]))
+# #print(findBall([[1]]))
+# print(findBall([[-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,1,1,-1,-1,-1,1,1,1,-1,-1,1,1,-1,-1,1,-1,-1,-1,-1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,-1,-1,-1,1,-1,-1,1,-1,1,-1,-1,1,1,-1,1,-1,-1,-1,-1,1,1,1,1,1,1,-1,1,1,1,-1,1,1,1,-1,-1,-1,1,-1,1,-1,-1,1,1,-1,-1,1,-1,1,-1,1,1,1,-1,-1,-1,-1]]))
