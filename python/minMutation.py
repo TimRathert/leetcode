393a783f7ac1dc162a98a6496f3da2b4d6eb2942
TimRathert <rathertt@gmail.com>
2022-11-02 10:49:55 -0400

    refactored minmutations

--- python/minMutation.py
+++ python/minMutation.py
@@ -1,42 +1,32 @@
 def minMutation(start, end, bank):
-    diffObj = {}
     totalChanges = 0
-    if len(bank) == 0 and start != end:
-        return -1
-    for index, letter in enumerate(start):
-        if letter != end[index]:
-            totalChanges += 1
-    for index, word in enumerate(bank):
-        startDiff = 0
-        endDiff = 0
-        for idx, letter, in enumerate(word):
-            if letter != start[idx]:
-                startDiff += 1
-            if letter != end[idx]:
-                endDiff += 1
-        diffObj[startDiff] = {'endDiff': endDiff, 'index': index}
-    if len(bank) == 1 and diffObj[1]['endDiff'] == 0:
-        return 1
-    # print(diffObj)
-    countdownVar = diffObj[1]['endDiff']
-    minMut = len(bank)
-    for i in range(1, len(bank), 1):
-        try:
-            diffObj[i]
-        except:
-            return -1
-        
-        else: 
-            if diffObj[i]['endDiff'] == 0 and i < minMut:
-                minMut = i
-            if bank[i-1] == end and i > totalChanges:
-                return i
+    while start != end:
+        if len(bank) < 1:
+            if not bank or bank[0] != end:
+                return -1
+            elif bank[0] == end:
+                return 1
 
-            if diffObj[i]['endDiff'] > countdownVar:
+        for item in bank:
+            tally = 0
+            for index, letter in enumerate(item):
+                #print(letter, start[index])
+                if letter != start[index]:
+                    tally += 1
+            if tally == 1:
+                #print(start, item)
+                start = item
+                bank.remove(item)
+                totalChanges += 1
+                # continue
+            if start == end:
+                return totalChanges
+            if tally > len(bank):
                 return -1
 
+    return totalChanges
 
-    return minMut if minMut > 0 else len(bank)
+        
 
 
 
