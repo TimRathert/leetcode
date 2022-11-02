76a44de9c9d3fb0c09ffe2d4d866927eb84dd13f
TimRathert <rathertt@gmail.com>
2022-11-02 11:50:15 -0400

    taking a break

--- python/minMutation.py
+++ python/minMutation.py
@@ -1,5 +1,8 @@
 def minMutation(start, end, bank):
     totalChanges = 0
+    if end not in bank:
+        return -1
+    previousEndDiff = None
     while start != end:
         if len(bank) < 1:
             if not bank or bank[0] != end:
@@ -8,17 +11,36 @@ def minMutation(start, end, bank):
                 return 1
 
         for item in bank:
+            if item == start:
+                bank.remove(start)
+                break
+            endDiff = 0
             tally = 0
             for index, letter in enumerate(item):
                 #print(letter, start[index])
                 if letter != start[index]:
                     tally += 1
-            #print(totalChanges, len(bank),tally)
-            if tally == 1:
-                print(start, item)
-                start = item
-                bank.remove(item)
-                totalChanges += 1
+
+                if start[index] != end[index]:
+                    endDiff += 1
+            if previousEndDiff != None:    
+                if endDiff > previousEndDiff:
+                    start = previousItem
+                    bank.append(previousItem)
+                    bank.remove(item)
+                    break    
+            previousEndDiff = endDiff
+            previousItem = item
+            #print(tally, len(bank), endDiff)
+            if tally <= 1:
+                if endDiff == 1 and item != end:
+                    continue
+                else:
+                    #print(start, item)
+                    start = item
+                    bank.remove(item)
+                    totalChanges += 1
+                    endDiff -= 1
                 if start == end:
                     return totalChanges
                 else:
@@ -32,15 +54,17 @@ def minMutation(start, end, bank):
         
 
 
-
-#print(minMutation("AACCGGTT","AACCGGTA",["AACCGGTA"]))
-#print(minMutation("AACCGGTT","AAACGGTA",["AACCGGTA","AACCGCTA","AAACGGTA"]))
-#print(minMutation("AAAAACCC","AACCCCCC",["AAAACCCC","AAACCCCC","AACCCCCC"]))
-#print(minMutation("AACCTTGG","AATTCCGG",["AATTCCGG","AACCTGGG","AACCCCGG","AACCTACC"]))
-#print(minMutation("AACCGGTT","AACCGGTA",[]))
-#print(minMutation("AACCGGTT","AACCGGTA",["AACCGGTA","AACCGCTA","AAACGGTA"]))
-#print(minMutation("AACCGGTT","AAACGGTA",["AACCGATT","AACCGATA","AAACGATA","AAACGGTA"]))
+print(minMutation("AACCGGTT","AACCGGTA",["AACCGGTA"]))
+print(minMutation("AACCGGTT","AAACGGTA",["AACCGGTA","AACCGCTA","AAACGGTA"]))
+print(minMutation("AAAAACCC","AACCCCCC",["AAAACCCC","AAACCCCC","AACCCCCC"]))
+print(minMutation("AACCTTGG","AATTCCGG",["AATTCCGG","AACCTGGG","AACCCCGG","AACCTACC"]))
+print(minMutation("AACCGGTT","AACCGGTA",[]))
+print(minMutation("AACCGGTT","AACCGGTA",["AACCGGTA","AACCGCTA","AAACGGTA"]))
+print(minMutation("AACCGGTT","AAACGGTA",["AACCGATT","AACCGATA","AAACGATA","AAACGGTA"]))
 print(minMutation("AACCGGTT","AACCGCTA",["AACCGGTA","AACCGCTA","AAACGGTA"]))
+print(minMutation("AAAAAAAA","CCCCCCCC",["AAAAAAAA","AAAAAAAC","AAAAAACC","AAAAACCC","AAAACCCC","AACACCCC","ACCACCCC","ACCCCCCC","CCCCCCCA"]))
+print(minMutation("AAAAAAAA","CCCCCCCC",["AAAAAAAA","AAAAAAAC","AAAAAACC","AAAAACCC","AAAACCCC","AACACCCC","ACCACCCC","ACCCCCCC","CCCCCCCA","CCCCCCCC"]))
+print(minMutation("AAAACCCC","CCCCCCCC",["AAAACCCA","AAACCCCA","AACCCCCA","AACCCCCC","ACCCCCCC","CCCCCCCC","AAACCCCC","AACCCCCC"]))
 
 
 # def minMutation(start, end, bank):
