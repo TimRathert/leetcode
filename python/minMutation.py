2d057804bd4877d96de16b4efbfac6a199dcc3df
TimRathert <rathertt@gmail.com>
2022-11-02 17:19:11 -0400

    bah!

--- python/minMutation.py
+++ python/minMutation.py
@@ -1,55 +1,27 @@
+from collections import deque
 def minMutation(start, end, bank):
-    totalChanges = 0
+    bank = set(bank)
     if end not in bank:
         return -1
-    previousEndDiff = None
-    while start != end:
-        if len(bank) < 1:
-            if not bank or bank[0] != end:
-                return -1
-            elif bank[0] == end:
-                return 1
+    def checkNeighbor(a,b):
+        return sum([1 for i in range(len(a)) if a[i]!=b[i]]) == 1
+    visited = set()
+    mutations = 0
+    queue = deque([start])
+    while queue:
+        for _ in range (len(queue)):
+            cur = queue.popleft()
+            if cur == end:
+                return mutations
+            if cur not in visited:
+                visited.add(cur)
+                for nei in bank:
+                    if checkNeighbor(cur,nei):
+                        queue.append(nei)
+        mutations += 1
+    return -1
 
-        for item in bank:
-            if item == start:
-                bank.remove(start)
-                break
-            endDiff = 0
-            tally = 0
-            for index, letter in enumerate(item):
-                #print(letter, start[index])
-                if letter != start[index]:
-                    tally += 1
-
-                if start[index] != end[index]:
-                    endDiff += 1
-            if previousEndDiff != None:    
-                if endDiff > previousEndDiff:
-                    start = previousItem
-                    bank.append(previousItem)
-                    bank.remove(item)
-                    break    
-            previousEndDiff = endDiff
-            previousItem = item
-            #print(tally, len(bank), endDiff)
-            if tally <= 1:
-                if endDiff == 1 and item != end:
-                    continue
-                else:
-                    #print(start, item)
-                    start = item
-                    bank.remove(item)
-                    totalChanges += 1
-                    endDiff -= 1
-                if start == end:
-                    return totalChanges
-                else:
-                    continue
-            if tally > len(bank):
-                #print('beans')
-                return -1
-
-    return totalChanges
+    
 
         
 
@@ -117,3 +89,55 @@ print(minMutation("AAAACCCC","CCCCCCCC",["AAAACCCA","AAACCCCA","AACCCCCA","AACCC
 # print(minMutation("AACCGGTT","AACCGGTA",["AACCGGTA","AACCGCTA","AAACGGTA"]))
 # print(minMutation("AACCGGTT","AAACGGTA",["AACCGATT","AACCGATA","AAACGATA","AAACGGTA"]))
 
+# def minMutation(start, end, bank):
+#     totalChanges = 0
+#     if end not in bank:
+#         return -1
+#     previousEndDiff = None
+#     while start != end:
+#         if len(bank) < 1:
+#             if not bank or bank[0] != end:
+#                 return -1
+#             elif bank[0] == end:
+#                 return 1
+
+#         for item in bank:
+#             if item == start:
+#                 bank.remove(start)
+#                 break
+#             endDiff = 0
+#             tally = 0
+#             for index, letter in enumerate(item):
+#                 #print(letter, start[index])
+#                 if letter != start[index]:
+#                     tally += 1
+
+#                 if start[index] != end[index]:
+#                     endDiff += 1
+#             if previousEndDiff != None:    
+#                 if endDiff > previousEndDiff:
+#                     start = previousItem
+#                     bank.append(previousItem)
+#                     bank.remove(item)
+#                     break    
+#             previousEndDiff = endDiff
+#             previousItem = item
+#             #print(tally, len(bank), endDiff)
+#             if tally <= 1:
+#                 if endDiff == 1 and item != end:
+#                     continue
+#                 else:
+#                     #print(start, item)
+#                     start = item
+#                     bank.remove(item)
+#                     totalChanges += 1
+#                     endDiff -= 1
+#                 if start == end:
+#                     return totalChanges
+#                 else:
+#                     continue
+#             if tally > len(bank):
+#                 #print('beans')
+#                 return -1
+
+#     return totalChanges
