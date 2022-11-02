ec7ce0fbaebe8cd1e0078d296b701c1e5370aaa2
TimRathert <rathertt@gmail.com>
2022-11-02 09:59:38 -0400

    refactoring minmut

--- python/minMutation.py
+++ python/minMutation.py
@@ -14,11 +14,10 @@ def minMutation(start, end, bank):
                 startDiff += 1
             if letter != end[idx]:
                 endDiff += 1
-            print(index, idx, startDiff, endDiff)
         diffObj[startDiff] = {'endDiff': endDiff, 'index': index}
     if len(bank) == 1 and diffObj[1]['endDiff'] == 0:
         return 1
-    print(diffObj)
+    # print(diffObj)
     countdownVar = diffObj[1]['endDiff']
     minMut = len(bank)
     for i in range(1, len(bank), 1):
@@ -37,15 +36,66 @@ def minMutation(start, end, bank):
                 return -1
 
 
-    return minMut
+    return minMut if minMut > 0 else len(bank)
 
 
 
-#print(minMutation("AACCGGTT","AACCGGTA",["AACCGGTA"]))
-#print(minMutation("AACCGGTT","AAACGGTA",["AACCGGTA","AACCGCTA","AAACGGTA"]))
-#print(minMutation("AAAAACCC","AACCCCCC",["AAAACCCC","AAACCCCC","AACCCCCC"]))
-#print(minMutation("AACCTTGG","AATTCCGG",["AATTCCGG","AACCTGGG","AACCCCGG","AACCTACC"]))
-#print(minMutation("AACCGGTT","AACCGGTA",[]))
-#print(minMutation("AACCGGTT","AACCGGTA",["AACCGGTA","AACCGCTA","AAACGGTA"]))
+print(minMutation("AACCGGTT","AACCGGTA",["AACCGGTA"]))
+print(minMutation("AACCGGTT","AAACGGTA",["AACCGGTA","AACCGCTA","AAACGGTA"]))
+print(minMutation("AAAAACCC","AACCCCCC",["AAAACCCC","AAACCCCC","AACCCCCC"]))
+print(minMutation("AACCTTGG","AATTCCGG",["AATTCCGG","AACCTGGG","AACCCCGG","AACCTACC"]))
+print(minMutation("AACCGGTT","AACCGGTA",[]))
+print(minMutation("AACCGGTT","AACCGGTA",["AACCGGTA","AACCGCTA","AAACGGTA"]))
 print(minMutation("AACCGGTT","AAACGGTA",["AACCGATT","AACCGATA","AAACGATA","AAACGGTA"]))
+
+
+# def minMutation(start, end, bank):
+#     diffObj = {}
+#     totalChanges = 0
+#     if len(bank) == 0 and start != end:
+#         return -1
+#     for index, letter in enumerate(start):
+#         if letter != end[index]:
+#             totalChanges += 1
+#     for index, word in enumerate(bank):
+#         startDiff = 0
+#         endDiff = 0
+#         for idx, letter, in enumerate(word):
+#             if letter != start[idx]:
+#                 startDiff += 1
+#             if letter != end[idx]:
+#                 endDiff += 1
+#         diffObj[startDiff] = {'endDiff': endDiff, 'index': index}
+#     if len(bank) == 1 and diffObj[1]['endDiff'] == 0:
+#         return 1
+#     # print(diffObj)
+#     countdownVar = diffObj[1]['endDiff']
+#     minMut = len(bank)
+#     for i in range(1, len(bank), 1):
+#         try:
+#             diffObj[i]
+#         except:
+#             return -1
+        
+#         else: 
+#             if diffObj[i]['endDiff'] == 0 and i < minMut:
+#                 minMut = i
+#             if bank[i-1] == end and i > totalChanges:
+#                 return i
+
+#             if diffObj[i]['endDiff'] > countdownVar:
+#                 return -1
+
+
+#     return minMut if minMut > 0 else len(bank)
+
+
+
+# print(minMutation("AACCGGTT","AACCGGTA",["AACCGGTA"]))
+# print(minMutation("AACCGGTT","AAACGGTA",["AACCGGTA","AACCGCTA","AAACGGTA"]))
+# print(minMutation("AAAAACCC","AACCCCCC",["AAAACCCC","AAACCCCC","AACCCCCC"]))
+# print(minMutation("AACCTTGG","AATTCCGG",["AATTCCGG","AACCTGGG","AACCCCGG","AACCTACC"]))
+# print(minMutation("AACCGGTT","AACCGGTA",[]))
+# print(minMutation("AACCGGTT","AACCGGTA",["AACCGGTA","AACCGCTA","AAACGGTA"]))
+# print(minMutation("AACCGGTT","AAACGGTA",["AACCGATT","AACCGATA","AAACGATA","AAACGGTA"]))
 
