43759ab201676b931221683dca6818b1f2645f25
TimRathert <rathertt@gmail.com>
2022-11-03 13:17:01 -0400

    longest palindrome

--- python/longestPalindrome.py
+++ python/longestPalindrome.py
@@ -1,29 +1,33 @@
+from collections import defaultdict
+
 def longestPalindrome(words):
-    copyPasta = words
-    needPair = {'dupes':[]}
-    output = 0
-    for index, item in enumerate(words):
-        needPair[item] = index
-    for i in range(len(words)-1):      
-        try:
-            needPair[item[::-1]]
-        except:
-            None
-        else:
-           
-            #print(index, item)
-    #print(needPair)
-    for item in needPair:
+    unpaired = output = 0
+    needPair = defaultdict(int)
+    for item in words:
         if item[0] == item[1]:
-            needPair.pop(item)
-            output += 2
-            break
+            if needPair[item] > 0:
+                unpaired -= 1
+                needPair[item] -= 1
+                output += 4
+            else:
+                needPair[item] += 1
+                unpaired += 1
+        else:
+            if needPair[item[::-1]] > 0:
+                output += 4
+                needPair[item[::-1]] -= 1
+            else:
+                needPair[item] += 1
+    if unpaired > 0: output += 2
     return output
+        
+        
 
 
 
-#print(longestPalindrome(["lc","cl","gg"])) 
-#print(longestPalindrome(["ab","ty","yt","lc","cl","ab"])) 
-#print(longestPalindrome(["cc","ll","xx"])) 
+print(longestPalindrome(["lc","cl","gg"])) 
+print(longestPalindrome(["ab","ty","yt","lc","cl","ab"])) 
+print(longestPalindrome(["cc","ll","xx"])) 
 print(longestPalindrome(["qo","fo","fq","qf","fo","ff","qq","qf","of","of","oo","of","of","qf","qf","of"])) 
 
+["qo","fo","ff","qq","qf","of","oo","of","of","qf","qf","of"]
